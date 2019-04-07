4e38fdf6ef282757d93fa47821bec1a1d8ce4cc5
mbadar6398 <mbadar6398@gmail.com>
2019-04-07 15:03:28 +0700

    features/create-topic add back again topic js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CreateTopic from './CreateTopic';
+import Topic from './Topic';
 
 class App extends Component {
     render() {
@@ -9,6 +10,13 @@ class App extends Component {
                     <div className="row justify-content-center">
                         <div className="col-12 col-md-8 col-lg-6">
                             <CreateTopic/>
+                        </div>
+                    </div>
+                </section>
+                <section className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-md-8 col-lg-6">
+                            <Topic/>
                         </div>
                     </div>
                 </section>
