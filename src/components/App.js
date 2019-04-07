7945e555d70b95797b9ba306f950e8378a45afa4
mbadar6398 <mbadar6398@gmail.com>
2019-04-07 14:41:24 +0700

    features/create-topic delete topic.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CreateTopic from './CreateTopic';
-import Topic from './Topic';
 
 class App extends Component {
     render() {
@@ -10,13 +9,6 @@ class App extends Component {
                     <div className="row justify-content-center">
                         <div className="col-12 col-md-8 col-lg-6">
                             <CreateTopic/>
-                        </div>
-                    </div>
-                </section>
-                <section className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-12 col-md-8 col-lg-6">
-                            <Topic/>
                         </div>
                     </div>
                 </section>
