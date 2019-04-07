dbb7022762d8b32e2a78c04d0e3b2d168f2170bd
mbadar6398 <mbadar6398@gmail.com>
2019-04-07 14:43:32 +0700

    features/top remove createtopic.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import CreateTopic from './CreateTopic';
 import Topic from './Topic';
 
 class App extends Component {
     render() {
         return(
             <main>
-                <section className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-12 col-md-8 col-lg-6">
-                            <CreateTopic/>
-                        </div>
-                    </div>
-                </section>
                 <section className="container">
                     <div className="row justify-content-center">
                         <div className="col-12 col-md-8 col-lg-6">
